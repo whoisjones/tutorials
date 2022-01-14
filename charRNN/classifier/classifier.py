57dbd069d23846ad6cf580ce6bdb6a2644740e37
jgolde <jonas.golde@gmail.com>
2022-01-14 07:10:01 +0100

    using of torch modules

--- charRNN/classifier/classifier.py
+++ charRNN/classifier/classifier.py
@@ -1,23 +1,30 @@
 import torch
+from torch.nn import RNN, LSTM, GRU
 
 
-class RNN(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
+class RNNModel(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, rnn_type: str = "RNN"):
+        super(RNNModel, self).__init__()
 
-        self.hidden_size = hidden_size
-
-        self.input2hidden = torch.nn.Linear(input_size + hidden_size, hidden_size)
-        self.input2output = torch.nn.Linear(input_size + hidden_size, output_size)
+        if rnn_type == "RNN":
+            self.rnn_layer = RNN(input_size=input_size, hidden_size=hidden_size)
+        elif rnn_type == "LSTM":
+            self.rnn_layer = LSTM(input_size=input_size, hidden_size=hidden_size)
+        else:
+            self.rnn_layer = GRU(input_size=input_size, hidden_size=hidden_size)
+        self.linear = torch.nn.Linear(hidden_size, output_size)
+        self.dropout = torch.nn.Dropout(p=0.1)
         self.softmax = torch.nn.LogSoftmax(dim=1)
         self.criterion = torch.nn.NLLLoss()
 
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.input2hidden(combined)
-        output = self.input2output(combined)
-        output = self.softmax(output)
-        return output, hidden
+    def forward(self, input):
+        output, hn = self.rnn_layer(input)
+        logits = self.linear(output[-1])
+        pred = self.softmax(logits)
+        return pred
+
+    def predict(self, output):
+        return self.softmax(output)
 
     def initHidden(self):
         return torch.zeros(1, self.hidden_size)
--- charRNN/classifier/train.py
+++ charRNN/classifier/train.py
@@ -1,6 +1,6 @@
 from dataloader import get_data, random_training_example
 from preprocess import n_letters
-from classifier import RNN
+from classifier import RNNModel
 from evaluation import category_from_output, time_since
 
 import time
@@ -11,7 +11,7 @@ def main():
     category_lines, all_categories = get_data(path)
 
     hidden_size = 128
-    rnn = RNN(input_size=n_letters, hidden_size=hidden_size, output_size=len(all_categories))
+    rnn = RNNModel(input_size=n_letters, hidden_size=hidden_size, output_size=len(all_categories), rnn_type="GRU")
 
     n_iters = 100000
     print_every = 5000
@@ -19,7 +19,7 @@ def main():
     start = time.time()
     for iter in range(1, n_iters + 1):
         category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
-        output, loss = train(rnn, category_tensor, line_tensor)
+        output, loss = train(rnn, line_tensor, category_tensor)
         current_loss += loss
 
         if iter % print_every == 0:
@@ -29,23 +29,19 @@ def main():
             iter, iter / n_iters * 100, time_since(start), loss, line, guess, correct))
 
 
-def train(rnn, category_tensor, line_tensor):
-    hidden = rnn.initHidden()
-
+def train(rnn, line_tensor, category_tensor):
     rnn.zero_grad()
 
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
+    pred = rnn(line_tensor)
 
-    loss = rnn.criterion(output, category_tensor)
+    loss = rnn.criterion(pred, category_tensor)
     loss.backward()
 
     # Add parameters' gradients to their values, multiplied by learning rate
     for p in rnn.parameters():
         p.data.add_(p.grad.data, alpha=-learning_rate)
 
-    return output, loss.item()
-
+    return pred, loss.item()
 
 if __name__ == "__main__":
     main()
